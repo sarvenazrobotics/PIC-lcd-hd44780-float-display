e86bfbf605d64d8146ae9de724efde3ffed2407e
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-04-20 14:08:58 +0200

    pic18f4550 intializes lcd display

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: Sarvenaz
+ *
+ * Created on April 20, 2026, 2:04 PM
+ */
+
+
+#include "xc.h"
+#include <stdlib.h>
+#include "xlcd.h"
+
+#pragma config FOSC= INTOSCIO_EC
+#pragma config WDT=OFF
+#pragma config LVP=OFF
+#pragma config MCLRE=ON
+#pragma config PBADEN=OFF
+
+int main(void) {
+    return 0;
+}
