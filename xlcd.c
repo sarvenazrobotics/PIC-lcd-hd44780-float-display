d6cb753cdc29d4ced827d4986257c1aae6c24bf4
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-04-20 14:01:45 +0200

    pic18f4550 intializes lcd display

---
+++ xlcd.c
@@ -0,0 +1,13 @@
+/*
+ * File:   LCD.c
+ * Author: Sarve
+ *
+ * Created on April 20, 2026, 1:32 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ xlcd.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   xlcd.h
+ * Author: Sarve
+ *
+ * Created on April 20, 2026, 1:46 PM
+ */
+
+#ifndef XLCD_H
+#define	XLCD_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* XLCD_H */
+
